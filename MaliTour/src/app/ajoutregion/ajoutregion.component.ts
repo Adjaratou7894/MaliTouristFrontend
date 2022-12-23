41a60e7fa8e0bc7e12d1d00bcac1e1c4a8a6b52e
Adjaratou7894 <adjarasira@gmail.com>
2022-12-23 08:36:42 +0000

    ajout des differents composants

---
+++ MaliTour/src/app/ajoutregion/ajoutregion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajoutregion',
+  templateUrl: './ajoutregion.component.html',
+  styleUrls: ['./ajoutregion.component.scss']
+})
+export class AjoutregionComponent {
+
+}
--- MaliTour/src/app/app.module.ts
+++ MaliTour/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { RegionComponent } from './region/region.component';
+import { DetailRegionComponent } from './detail-region/detail-region.component';
+import { AjoutregionComponent } from './ajoutregion/ajoutregion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    AccueilComponent,
+    InscriptionComponent,
+    RegionComponent,
+    DetailRegionComponent,
+    AjoutregionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
